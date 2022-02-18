1577a62cf20283a74bab6de110244facc8deec2e
camilaalmeidaa <99203530+camilaalmeidaa@users.noreply.github.com>
2022-02-18 14:50:58 -0300

    Add files via upload

---
+++ exe1.py
@@ -0,0 +1,11 @@
+def nums():
+    
+    numeros = []
+    
+    for i in range(5):
+        
+        num = int(input("*Exercicio1* Digite um numero inteiro: "))
+        numeros.append(num)
+        
+    
+    print("Os numeros são:",numeros)
