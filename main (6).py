1228a5ed3796ff660388b75c585452e703780732
camilaalmeidaa <99203530+camilaalmeidaa@users.noreply.github.com>
2022-03-18 15:36:53 -0300

    Add files via upload

---
+++ main (6).py
@@ -0,0 +1,47 @@
+import exe1
+import exe2
+import exe3
+import exe4
+import exe5
+import exe6
+import exe7
+import exe8
+import exe9
+import exe10
+import exe11
+import exe12
+import exe13
+import exe14
+import exe15
+import exe16
+import exe17
+import exe18
+import exe19
+import exe20
+import exe21
+import exe22
+import exe24
+
+exe1.nums([1,2,3,4,5])
+exe2.lista_inversa([1,2,3,4,5,6,7,8,9,10])
+exe3.notas()
+consoantes(['a','b','c','d','e','f','g','h','i','j'])
+exe5.par_impar()
+exe6.notas2()
+exe7.numeros([1,2,3,4,5])
+exe8.idade_altura()
+exe9.soma_quadrados([1,2,3,4,5,6,7,8,9,10])
+exe10.vetores()
+exe11.vetores2()
+exe12.idade_altura2()
+exe13.temperatura()
+exe14.perguntas()
+exe15.notas3()
+exe16.vendedores()
+exe17.saltos()
+exe18.jogadores()
+exe19.sistemas()
+exe20.abonos()
+exe21.combustivel(['Fusca','Gol','Uno','Vectra','Peugeot'],[7,10,12.5,9,14.5])
+exe22.mouses()
+exe24.dados()
